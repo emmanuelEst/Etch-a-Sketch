1233535af8f6c12ebef8f58a1cbbf2c13818c72f
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-06 10:51:57 -0500

    Create function that builds the grid
    The buildGrid function is called when the user has entered a number after
    pressing the button on the page.

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ function setColumnsAndRows() {
     } else if (userInput > 100 || userInput < 1) {
         alert('Columns and Rows need to be between numbers 1 through 100')
     } else {
-        // buildGrid(Math.Floor(userInput));
-        console.log('Build Grid Function');
+        buildGrid(Math.floor(userInput));
     }
+}
+
+// builds rows and columns
+function buildGrid(gridSize) {
+    let sketchPad = document.querySelector('#sketch-pad');
+    let newDiv;
+
+    for (let i = 1; i <= (gridSize ** 2); i++) {
+        newDiv = document.createElement('div');
+        sketchPad.appendChild(newDiv);
+    }
+
+    sketchPad.style.cssText = `grid-template: repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr);`;
+
 }
